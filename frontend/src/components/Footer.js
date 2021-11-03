1feb9849975141a23842d5a461e78076ab08159a
devraj2018 <shekhawatdevraj123@gmail.com>
2021-11-04 00:37:34 +0530

    BasicFrontEndCompleted

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Col, Container, Row} from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>
+                        Copyright &copy; Proshop
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+            
+    
+        )
+}
+
+export default Footer
