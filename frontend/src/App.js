089b82b50a42d841adebaf36ad87f080f1872c6b
devraj2018 <shekhawatdevraj123@gmail.com>
2021-11-07 00:08:17 +0530

    [Updated Upto Register Screen Functionality]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,8 @@ import Footer from './components/Footer'
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
-import LoginScreen from "./screens/LoginScreen";
+import LoginScreen from "./screens/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen"
 const App= ()=> {
   return (
     <Router>
@@ -14,6 +15,7 @@ const App= ()=> {
       <main className='py-3'>
         <Container>
          <Route path='/login' component={LoginScreen}/>
+         <Route path='/register' component={RegisterScreen}/>
          <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/> 
            <Route path='/' component={HomeScreen} exact/>  
